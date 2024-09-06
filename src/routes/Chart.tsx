97d248c5c7d9775a03b31655af918ad8d08e55ce
heejun <heejun.han@gmail.com>
2024-09-06 17:38:57 +0900

    upload files

---
+++ src/routes/Chart.tsx
@@ -0,0 +1,74 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchCoinHistory } from "../api";
+import ReactApexChart from "react-apexcharts";
+import { useRecoilValue } from "recoil";
+import { isDartAtom } from "../atoms";
+
+interface IHistorical {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
+
+interface ChartProps {
+  coinId: string;
+}
+
+function Chart({ coinId }: ChartProps) {
+  const isDark = useRecoilValue(isDartAtom);
+  const { isLoading, data } = useQuery<IHistorical[]>({
+    queryKey: ["ohlcv", coinId],
+    queryFn: () => fetchCoinHistory(coinId),
+    refetchInterval: 5000,
+  });
+
+  return (
+    <div>
+      {isLoading ? (
+        "Loading chart..."
+      ) : (
+        <ReactApexChart
+          type="candlestick"
+          height={350}
+          series={[
+            {
+              data:
+                data?.map((price) => ({
+                  x: price.time_close,
+                  y: [price.open, price.high, price.low, price.close],
+                })) ?? [],
+            },
+          ]}
+          options={{
+            theme: {
+              mode: isDark ? "dark" : "light",
+            },
+            chart: {
+              type: "candlestick",
+              height: 350,
+            },
+            title: {
+              text: "CandleStick Chart",
+              align: "left",
+            },
+            xaxis: {
+              type: "datetime",
+            },
+            yaxis: {
+              tooltip: {
+                enabled: true,
+              },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
+}
+
+export default Chart;
---
+++ src/theme.ts
@@ -0,0 +1,23 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "#2f3640",
+  childBgColor: "#000000",
+  textColor: "#f5f6fa",
+  accentColor: "#9c88ff",
+};
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "#f5f6fa",
+  childBgColor: "#dcdde1",
+  textColor: "#000000",
+  accentColor: "#c23616",
+};
+
+// export interface ThemeMode {
+//   theme: DefaultTheme;
+// }
+
+// export const themeMode: ThemeMode = {
+//   theme: lightTheme,
+// };
